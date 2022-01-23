812ded98873a90180d6402086a67b0c8bf54df10
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2022-01-23 22:27:23 +0700

    Create Project Android Dasar

---
+++ app/src/main/java/programmerzamannow/belajarandroiddasar/MainActivity.kt
@@ -0,0 +1,20 @@
+package programmerzamannow.belajarandroiddasar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.hello_world)
+
+        val nameEditText: EditText = findViewById(R.id.nameEditText)
+        val sayHelloButton: Button = findViewById(R.id.sayHelloButton)
+        val sayHelloTextView: TextView = findViewById(R.id.sayHelloTextView)
+
+        sayHelloTextView.text = "Hi"
+    }
+}
